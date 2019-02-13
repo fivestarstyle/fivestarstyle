de505f179ecde8f1f235adf9246a4ea9c8ae8e39
Abigail Oswald <aoswald2@crimson.ua.edu>
2019-02-13 16:45:34 -0600

    added buttons and title

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
@@ -12,12 +12,19 @@ import lecho.lib.hellocharts.model.PieChartData;
 import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.view.PieChartView;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 public class MainActivity<pieChartData> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_closet_statistics);
+        setContentView(R.layout.activity_main);
         createPieGraph();
     }
 
