d2d111cc6dada573370bc8f46043147d59b10132
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-02-14 14:40:52 -0600

    fixed issues after merge

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/LoginActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.fivestarstyle;
 
+import android.content.Intent;
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        MaterialButton registerScreen = (MaterialButton) findViewById(R.id.btnRegister);
+
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+                LoginActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
+
 }
--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
@@ -7,33 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 
 
-public class MainActivity<pieChartData> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MaterialButton signInScreen = (MaterialButton) findViewById(R.id.btnSignInScreen);
-
-        signInScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
-//                myIntent.putExtra("key", value); //Optional parameters
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-        MaterialButton registerScreen = (MaterialButton) findViewById(R.id.btnRegisterScreen);
-
-        registerScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
-//                myIntent.putExtra("key", value); //Optional parameters
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
 
 
     }
