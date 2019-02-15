86d8885a39c96ab3a4a4dfa56c387cdc59287ab0
meganwasserman <47225530+meganwasserman@users.noreply.github.com>
2019-02-15 08:39:34 -0600

    Add files via upload

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton overviewScreen = (ImageButton) findViewById(R.id.overview);
+
+        overviewScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent overviewIntent = new Intent(MainActivity.this, ClosetStatistics.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(overviewIntent);
+            }
+        });
 
 
     }
--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/RegisterActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.fivestarstyle;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        MaterialButton homeScreen = (MaterialButton) findViewById(R.id.btnRegister);
+
+        homeScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(RegisterActivity.this, MainActivity.class);
+//                myIntent.putExtra("key", value); //Optional parameters
+                RegisterActivity.this.startActivity(homeIntent);
+            }
+        });
     }
-}
+
+    }
+
