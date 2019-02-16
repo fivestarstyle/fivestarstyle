a82794d6ecb737352640820ab12d66483e41a747
Megan <mjwilliams1@crimson.ua.edu>
2019-02-16 09:57:50 -0600

    added dropdown menu to home and statistics screen.

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ClosetStatistics.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ClosetStatistics.java
@@ -1,5 +1,6 @@
 package com.example.fivestarstyle;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -156,5 +160,32 @@ public class ClosetStatistics<pieChartData> extends AppCompatActivity {
         pieChartView.setPieChartData(pieChartData);
 
         pieChartData.setSlicesSpacing(3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home__menu_option) {
+            Toast.makeText(this,"you pressed home!", Toast.LENGTH_LONG).show();
+            Intent homeIntent = new Intent(this,MainActivity.class);
+            this.startActivity(homeIntent);
+        }
+        else if(item.getItemId() == R.id.closet_menu_option) {
+            Toast.makeText(this,"you pressed closet!", Toast.LENGTH_LONG).show();
+        }
+        else if(item.getItemId() == R.id.overview_menu_option) {
+            Toast.makeText(this,"you pressed overview!", Toast.LENGTH_LONG).show();
+            Intent overviewIntent = new Intent(this,ClosetStatistics.class);
+            this.startActivity(overviewIntent);
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.view.Menu;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,5 +31,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home__menu_option) {
+            Toast.makeText(this,"you pressed home!", Toast.LENGTH_LONG).show();
+            Intent homeIntent = new Intent(this,MainActivity.class);
+            this.startActivity(homeIntent);
+        }
+        else if(item.getItemId() == R.id.closet_menu_option) {
+            Toast.makeText(this,"you pressed closet!", Toast.LENGTH_LONG).show();
+        }
+        else if(item.getItemId() == R.id.overview_menu_option) {
+            Toast.makeText(this,"you pressed overview!", Toast.LENGTH_LONG).show();
+            Intent overviewIntent = new Intent(this,ClosetStatistics.class);
+            this.startActivity(overviewIntent);
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
