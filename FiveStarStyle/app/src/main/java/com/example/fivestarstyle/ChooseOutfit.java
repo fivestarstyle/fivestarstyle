10a1bc00f6b1aa063010da59fb4f1a30485f9da6
azhall5032 <azhall@crimson.ua.edu>
2019-02-19 17:05:30 -0600

    add menu to other pages

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ChooseOutfit.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ChooseOutfit.java
@@ -1,15 +1,19 @@
 package com.example.fivestarstyle;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,34 @@ import java.util.List;
 public class ChooseOutfit extends AppCompatActivity {
     private ListView lstView;
     Dialog myDialog;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home__menu_option) {
+            Toast.makeText(this,"you pressed home!", Toast.LENGTH_LONG).show();
+            Intent homeIntent = new Intent(this,MainActivity.class);
+            this.startActivity(homeIntent);
+        }
+        else if(item.getItemId() == R.id.closet_menu_option) {
+            Toast.makeText(this,"you pressed closet!", Toast.LENGTH_LONG).show();
+        }
+        else if(item.getItemId() == R.id.overview_menu_option) {
+            Toast.makeText(this,"you pressed overview!", Toast.LENGTH_LONG).show();
+            Intent overviewIntent = new Intent(this,ClosetStatistics.class);
+            this.startActivity(overviewIntent);
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
