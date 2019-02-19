f77e4cfcafb54768a4edbc9199fd8afca82a61f0
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-02-19 16:30:41 -0600

    added camera functionality to closet screen

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/AddClothingActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/AddClothingActivity.java
@@ -1,7 +1,12 @@
 package com.example.fivestarstyle;
 
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddClothingActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class AddClothingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_clothing);
+        Button btnTakePicture = (Button) findViewById(R.id.btnCamera);
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ClosetActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ClosetActivity.java
@@ -1,7 +1,11 @@
 package com.example.fivestarstyle;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ClosetActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class ClosetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closet);
+
+        ImageButton addClothing = (ImageButton) findViewById(R.id.AddButton);
+
+        addClothing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
