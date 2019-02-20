413d44e2e45cf121fb7981ee89deebfb50f1adca
Abigail Oswald <aoswald2@crimson.ua.edu>
2019-02-20 10:32:24 -0600

    parsed weather json data and integrated it into choose outfit page

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ChooseOutfit.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/ChooseOutfit.java
@@ -1,22 +1,31 @@
 package com.example.fivestarstyle;
 
 import android.app.Dialog;
+import android.arch.core.util.Function;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -24,12 +33,17 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ChooseOutfit extends AppCompatActivity {
     private ListView lstView;
     Dialog myDialog;
     JSONObject data = null;
+
+    TextView currentTemp, humidity;
+    TextView weatherIcon;
+    Typeface weatherFont;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,6 +84,9 @@ public class ChooseOutfit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_outfit);
         getJSON("Tuscaloosa");
+        currentTemp = (TextView) findViewById(R.id.currentTemp);
+        humidity = (TextView) findViewById(R.id.humidity);
+        weatherIcon = (TextView) findViewById(R.id.weatherIcon);
     }
 
     public void getJSON(final String city) {
@@ -86,7 +103,7 @@ public class ChooseOutfit extends AppCompatActivity {
             @Override
             protected Void doInBackground(Void... params) {
                 try {
-                    URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=bba8e629ce93f0a063c0a46c47dc5960");
+                    URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=bba8e629ce93f0a063c0a46c47dc5960");
 
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -121,19 +138,57 @@ public class ChooseOutfit extends AppCompatActivity {
             protected void onPostExecute(Void Void) {
                 if(data!=null){
                     Log.d("my weather received",data.toString());
-//                    try {
-//                        JSONObject low = data.getJSONObject("temp_min");
-//                        JSONObject high = data.getJSONObject("temp_max");
-//                        JSONObject main = data.getJSONObject("main");
-//                    }
-//                    catch (JSONException e) {
-//                        // error handling
-//                    }
+                    try {
+                        JSONObject main = data.getJSONObject("main");
+                        JSONObject weather = data.getJSONArray("weather").getJSONObject(0);
+                        Log.d("weather returned",weather.toString());
+//                        String icon = weather.getString("icon");
+//                        String iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+//                        Picasso.get().load(iconUrl).into(weatherIcon);
+                        currentTemp.setText(String.format("%.0f",main.getDouble("temp")) + "°F");
+                        humidity.setText(String.format("%.0f",main.getDouble("temp_min")) + "%");
+                        weatherIcon.setText(Html.fromHtml(setWeatherIcon(weather.getInt("id"),data.getJSONObject("sys").getLong("sunrise") * 1000,
+                                data.getJSONObject("sys").getLong("sunset") * 1000)));
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), "Error, Check City", Toast.LENGTH_SHORT).show();
+                    }
+
+
                 }
 
             }
         }.execute();
 
+    }
+
+    public static String setWeatherIcon(int actualId, long sunrise, long sunset){
+        int id = actualId / 100;
+        String icon = "";
+        if(actualId == 800){
+            long currentTime = new Date().getTime();
+            if(currentTime>=sunrise && currentTime<sunset) {
+                icon = "&#xf00d;";
+            } else {
+                icon = "&#xf02e;";
+            }
+        } else {
+            switch(id) {
+                case 2 : icon = "&#xf01e;";
+                    break;
+                case 3 : icon = "&#xf01c;";
+                    break;
+                case 7 : icon = "&#xf014;";
+                    break;
+                case 8 : icon = "&#xf013;";
+                    break;
+                case 6 : icon = "&#xf01b;";
+                    break;
+                case 5 : icon = "&#xf019;";
+                    break;
+            }
+        }
+        return icon;
     }
 
 }
