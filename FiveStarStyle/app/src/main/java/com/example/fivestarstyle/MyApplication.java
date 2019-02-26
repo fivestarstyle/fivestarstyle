ef44db5d9badcbbddd3c9655ed0eadfbe0d2e24e
Abigail Oswald <aoswald2@crimson.ua.edu>
2019-02-26 12:09:03 -0600

    added city state and zip code options for location

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MyApplication.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/MyApplication.java
@@ -3,6 +3,9 @@ package com.example.fivestarstyle;
 import android.app.Application;
 
 public class MyApplication extends Application {
-    public static String longitude = "181";
-    public static String latitude = "91";
+    public static String longitude = "";
+    public static String latitude = "";
+    public static String city = "";
+    public static String state = "";
+    public static String zipCode = "";
 }
