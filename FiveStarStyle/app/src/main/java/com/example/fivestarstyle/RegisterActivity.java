87d5b59b884dd8165df00ff75e14637b9f5f66f3
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-02-26 13:37:28 -0600

    added fields to registrationto add user to database

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/RegisterActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/RegisterActivity.java
@@ -11,34 +11,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "Register";
     private FirebaseAuth mAuth;
     private EditText mEmailField;
     private EditText mPasswordField;
-    private View mProgressView;
-    private View mLoginFormView;
+    private EditText mFirstNameField;
+    private EditText mLastNameField;
+    private Spinner mGenderField;
+
+//    private View mProgressView;
+//    private View mLoginFormView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //initialize DropDown
+            Spinner spinner = (Spinner) findViewById(R.id.gender_spinner);
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.gender_array, android.R.layout.simple_spinner_item);
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
 
         mAuth = FirebaseAuth.getInstance();
 
         mEmailField = findViewById(R.id.txtEmail);
         mPasswordField = findViewById(R.id.txtPassword);
-        mProgressView = findViewById(R.id.login_progress);
+        mFirstNameField = findViewById(R.id.txtFirstName);
+        mLastNameField = findViewById(R.id.txtLastName);
+        mGenderField = findViewById(R.id.gender_spinner);
 
         MaterialButton btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +90,7 @@ public class RegisterActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            addUserCloset(user);
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -76,15 +100,36 @@ public class RegisterActivity extends AppCompatActivity {
                             updateUI(null);
                         }
 
-                        // [START_EXCLUDE]
-//                        showProgress(false);
-                        // [END_EXCLUDE]
                     }
                 });
-        // [END create_user_with_email]
+
     }
 
+    private void addUserCloset(FirebaseUser user){
+        if (user != null) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Map<String, Object> test = new HashMap<>();
+            test.put("first", mFirstNameField.getText().toString());
+            test.put("last", mLastNameField.getText().toString());
+            test.put("gender", mGenderField.getSelectedItem().toString());
 
+            // Add a new document with a generated ID
+            db.collection("userClosets" ).document(user.getUid())
+                    .set(test)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error writing document", e);
+                        }
+                    });
+        }
+    }
 
     private void updateUI(FirebaseUser user) {
 //        showProgress(false);
@@ -93,7 +138,7 @@ public class RegisterActivity extends AppCompatActivity {
 //                myIntent.putExtra("key", value); //Optional parameters
             RegisterActivity.this.startActivity(newIntent);
         } else {
-
+            Log.w(TAG, "Error changing screen");
         }
     }
 
@@ -120,32 +165,4 @@ public class RegisterActivity extends AppCompatActivity {
         return valid;
     }
 
-    private void showProgress(final boolean show) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
 }
