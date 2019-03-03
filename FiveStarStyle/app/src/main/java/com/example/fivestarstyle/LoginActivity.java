ac92c45c18f22a750d043d495fcdb13eee3ce6bc
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-03-03 12:45:25 -0600

    removed add clothing activity

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/LoginActivity.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/LoginActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
