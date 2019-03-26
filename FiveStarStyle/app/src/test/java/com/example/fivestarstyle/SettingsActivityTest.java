5f06950cac5ce9cfed309a0dd54b69bd643f6e60
Megan <mjwilliams1@crimson.ua.edu>
2019-03-26 12:14:20 -0500

    settings activity JUnit tests and new test configurations

---
+++ FiveStarStyle/app/src/test/java/com/example/fivestarstyle/SettingsActivityTest.java
@@ -0,0 +1,21 @@
+package com.example.fivestarstyle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SettingsActivityTest extends SettingsActivity {
+
+    String goodZip = "35205";
+    String nullZip = "";
+
+    @Test
+    public void customTrue() {
+        assertTrue(custom(goodZip));
+    }
+
+    @Test
+    public void customFalse() {
+        assertFalse(custom(nullZip));
+    }
+}
