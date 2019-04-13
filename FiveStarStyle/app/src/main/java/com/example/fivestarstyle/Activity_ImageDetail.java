d416e4cd10e47d3713e071da91d8892256c66cf6
Megan <mjwilliams1@crimson.ua.edu>
2019-04-13 17:09:43 -0500

    finishing touches on image details page. added menu. fixed strings

--- FiveStarStyle/app/src/main/java/com/example/fivestarstyle/Activity_ImageDetail.java
+++ FiveStarStyle/app/src/main/java/com/example/fivestarstyle/Activity_ImageDetail.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class Activity_ImageDetail extends AppCompatActivity {
+
+    private final static String TAG = "Activity_ImageDetails";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +69,24 @@ public class Activity_ImageDetail extends AppCompatActivity {
 
 
         seasons.setLength(seasons.length()-1);
-        seasonText.setText(String.valueOf(seasons));
+
+        String [] s = seasons.toString().split(",");
+        StringBuilder seasons2 = new StringBuilder();
+        String t;
+        seasons2.append(s[0].substring(0,1).toUpperCase()+s[0].substring(1));
+        if (s.length > 1) {
+            seasons2.append(", ");
+        }
+
+        for (int j = 1; j < s.length; j ++) {
+            t = s[j].substring(1,2).toUpperCase()+s[j].substring(2);
+            seasons2.append(t);
+            if ((j+1) < s.length) {
+                seasons2.append(", ");
+            }
+        }
+
+        seasonText.setText(String.valueOf(seasons2));
 
 
 
@@ -78,6 +100,71 @@ public class Activity_ImageDetail extends AppCompatActivity {
 
 
         events.setLength(events.length()-1);
-        eventText.setText(String.valueOf(events));
+        String [] x = events.toString().split(",");
+        StringBuilder events2 = new StringBuilder();
+        String y;
+        events2.append(x[0].substring(0,1).toUpperCase()+x[0].substring(1));
+        if (x.length > 1) {
+            events2.append(", ");
+        }
+
+        for (int i = 1; i < x.length; i ++) {
+            y = x[i].substring(1,2).toUpperCase()+x[i].substring(2);
+            events2.append(y);
+            if ((i+1) < x.length) {
+                events2.append(", ");
+            }
+        }
+        eventText.setText(String.valueOf(events2));
+    }
+
+
+    /*create options menu in upper right corner*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home__menu_option) {
+            Intent homeIntent = new Intent(this, Activity_Main.class);
+            this.startActivity(homeIntent);
+        }
+        else if(item.getItemId() == R.id.closet_menu_option) {
+            Intent overviewIntent = new Intent(this, Activity_ViewMyCloset.class);
+            this.startActivity(overviewIntent);
+        }
+        else if(item.getItemId() == R.id.add_menu_option) {
+            Intent overviewIntent = new Intent(this, Activity_GoogleCloudAPI.class);
+            this.startActivity(overviewIntent);
+        }
+        else if(item.getItemId() == R.id.overview_menu_option) {
+            Intent overviewIntent = new Intent(this, Activity_Overview.class);
+            this.startActivity(overviewIntent);
+        }
+        else if(item.getItemId() == R.id.choosemyoutfit_menu_option) {
+            Intent overviewIntent = new Intent(this, Activity_ChooseMyOutfit.class);
+            this.startActivity(overviewIntent);
+        }
+        else if(item.getItemId() == R.id.settings_menu_option) {
+            Intent overviewIntent = new Intent(this, Activity_Settings.class);
+            this.startActivity(overviewIntent);
+        }
+        else if(item.getItemId() == R.id.logout_menu_option) {
+            try {
+                FirebaseAuth.getInstance().signOut();
+                Log.d(TAG, "User signed out");
+                Intent overviewIntent = new Intent(this, Activity_Login.class);
+                this.startActivity(overviewIntent);
+            }catch (Exception e) {
+                Log.e(TAG, "onClick: Exception "+e.getMessage(),e );
+            }
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
