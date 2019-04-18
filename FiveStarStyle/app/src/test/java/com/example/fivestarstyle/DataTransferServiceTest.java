82c0042eaaeb0ed77435400b20ded5cfe665bc37
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-04-18 13:50:14 -0500

    adding powermock to gradle

--- FiveStarStyle/app/src/test/java/com/example/fivestarstyle/DataTransferServiceTest.java
+++ FiveStarStyle/app/src/test/java/com/example/fivestarstyle/DataTransferServiceTest.java
@@ -1,13 +1,23 @@
 package com.example.fivestarstyle;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
-
-import static org.junit.Assert.*;
-
-public class DataTransferServiceTest extends DataTransferService {
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(JUnit4.class)
+@PrepareForTest({FirebaseFirestore.class, FirebaseUser.class})
+public class DataTransferServiceTest extends DataTransferService{
+    //mock database reference and mock user
+    private FirebaseFirestore mockFirestore = Mockito.mock(FirebaseFirestore.class);
+    private FirebaseUser mockUser = Mockito.mock(FirebaseUser.class);
 
     @Test
     public void addItemTest() {
