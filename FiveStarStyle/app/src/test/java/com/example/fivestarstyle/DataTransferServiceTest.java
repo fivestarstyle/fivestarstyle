f5cf57f1fd5eb073d1e93b258775b0508e378770
Caley Curtis <cmcurtis1@crimson.ua.edu>
2019-04-18 12:07:11 -0500

    test start

---
+++ FiveStarStyle/app/src/test/java/com/example/fivestarstyle/DataTransferServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.fivestarstyle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataTransferServiceTest extends DataTransferService {
+
+    @Test
+    public void addItemTest() {
+    }
+
+    @Test
+    public void deleteItemTest() {
+    }
+
+    @Test
+    public void getCategoryCountTest() {
+    }
+}
